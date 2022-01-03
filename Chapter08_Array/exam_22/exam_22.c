ba110429adb3c3f7fc341c92fbdbac2222756c37
CM.Bae <gilddongii@gmail.com>
2022-01-03 21:50:59 +0900

    edit

--- Chapter08_Array/exam_22/exam_22.c
+++ Chapter08_Array/exam_22/exam_22.c
@@ -9,19 +9,46 @@ int main()
 	int gugu[9][9];
 	int i, k;
 
-	for (i = 1; i <= 9; i++)
+	//for (i = 1; i <= 9; i++)
+	//{
+	//	for (k = 1; k <= 9; k++)
+	//		printf(" %2dx%2d =%2d ", k, i, k * i);
+	//	printf("\n");
+	//}
+	//printf("\n");
+
+	//for (i = 1; i <= 9; i++)
+	//{
+	//	for (k = 1; k <= 9; k++)
+	//		printf(" %2dx%2d =%2d ", i, k, k * i);
+	//	printf("\n");
+	//}                                                // 배열을 이용하지 않았음
+
+	for (i = 0; i < 9; i++)
 	{
-		for (k = 1; k <= 9; k++)
-			printf(" %2dx%2d =%2d ", k, i, k * i);
+		for (k = 0; k < 9; k++)
+		{
+			gugu[i][k] = (i + 1) * (k + 1);
+		}
+	}
+
+	printf("구구단 출력\n");
+	for (i = 0; i < 9; i++)
+	{
+		for (k = 0; k < 9; k++)
+		{
+			printf("%dx%d=%2d ", i + 1, k + 1, gugu[i][k]);
+		}
 		printf("\n");
 	}
-	printf("\n");
+	printf("\n\n");
 
-	for (i = 1; i <= 9; i++)
+	for (i = 0; i < 9; i++)
 	{
-		for (k = 1; k <= 9; k++)
-			printf(" %2dx%2d =%2d ", i, k, k * i);
+		for (k = 0; k < 9; k++)
+		{
+			printf("%dx%d=%2d ", k + 1, i + 1, gugu[i][k]);
+		}
 		printf("\n");
 	}
-
 }
