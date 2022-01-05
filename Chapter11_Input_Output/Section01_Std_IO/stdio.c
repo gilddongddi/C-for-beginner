71685ded387218b1182fc4da50627d12b6725b83
CM.Bae <infanzia1012@naver.com>
2022-01-05 13:29:55 +0900

    getche(), getchar()

--- Chapter11_Input_Output/Section01_Std_IO/stdio.c
+++ Chapter11_Input_Output/Section01_Std_IO/stdio.c
@@ -66,35 +66,77 @@
 
 // 응용 11-4 문자 입출력 함수 사용 예 2 : getch()
 
-#include <stdio.h>
-#include <string.h>
+//#include <stdio.h>
+//#include <string.h>
+//
+//void main()
+//{
+//	char password[5] = "1234";		// {5,6,7,8,\0}  문자형은 null값 포함해서 5임  vs 정수형 배열은 null값 없음
+//	char input[5];
+//	int i;
+//
+//	//printf("%d\n", strcmp(password, input));
+//
+//	printf("비밀번호는 %s :\n", password);
+//
+//	printf("비밀번호 4 글자를 입력하세요 :");
+//	for (i = 0; i < 4; i++)
+//		//input[i] = getche();
+//		input[i] = getch();			// getch()함수는 문자를 한글자씩 보이지 않게 입력받음  vs getche()는 한글자씩 보여주면서 입력받음
+//
+//	printf("\n입력한 비밀번호는 %s \n", input);  // 안 된다. 입력한 비밀번호에 자꾸 쓰레기글자들이 낀다. 해결할 방법이 있을텐데?
+//
+//	if (strcmp(password, input) == 0) 
+//	{
+//		printf("\n비밀번호 일치합니다.\n");
+//	}
+//	else
+//	{
+//		printf("\n입력한 비밀번호 ");
+//
+//		for (i = 0; i < 4; i++)
+//			putch(input[i]);
+//		printf("가 틀렸습니다.\n");
+//	}
+//}
+
+
+// 기본 11-5 문자 입출력 함수 사용 예 3 , getche(); ==> 문자 한 글자씩 입력받고 모니터에 보여준다.
+
+//#include<stdio.h>
+//
+//void main()
+//{
+//	char ch;
+//
+//	ch = getche();
+//	ch = getche();
+//	ch = getche();
+//
+//}
+
+
+// 기본 11-6 문자 입출력 함수 사용 예 4, getchar();
+#include<stdio.h>
 
 void main()
 {
-	char password[5] = "1234";		// {5,6,7,8,\0}  문자형은 null값 포함해서 5임  vs 정수형 배열은 null값 없음
-	char input[5];
-	int i;
+	char ch;
 
-	//printf("%d\n", strcmp(password, input));
+	printf("문자열을 입력하세요 : ");
+	ch = getchar();			// 문자를 입력받아 버퍼(임시저장소)에 저장 후 첫 글자를 ch 변수에 저장
+	putchar(ch);			// ch 변수값 출력
 
-	printf("비밀번호는 %s :\n", password);
+	ch = getchar();			// 문자를 버퍼에서 한글자 빼서 ch에 저장
+	putchar(ch);			// ch 변수값 출력
 
-	printf("비밀번호 4 글자를 입력하세요 :");
-	for (i = 0; i < 4; i++)
-		input[i] = getche();			// getch()함수는 문자를 한글자씩 보이지 않게 입력받음  vs getche()는 한글자씩 보여주면서 입력받음
+	ch = getchar();			// 문자를 버퍼에서 한글자 빼서 ch에 저장
+	putchar(ch);			// ch 변수값 출력
 
-	printf("\n입력한 비밀번호는 %s \n", input);  // 안 된다. 입력한 비밀번호에 자꾸 쓰레기글자들이 낀다. 해결할 방법이 있을텐데?
+	ch = getchar();			// 만약 여기서(3글자만 입력 후) 엔터키를 치면 , 엔터키값이 버퍼에 저장된 후 ch변수에 대입되고 출력 => 줄바뀜
+	putchar(ch);
 
-	if (strcmp(password, input) == 0) 
-	{
-		printf("\n비밀번호 일치합니다.\n");
-	}
-	else
-	{
-		printf("\n입력한 비밀번호 ");
+	ch = getchar();			// 여기에서 한글자를 더 입력받을 수 있아 출력할 수 있음. 그냥 엔터키를 치켠 엔터키값이 저장된 후 반환되서 줄바뀌고 끝.
+	putchar(ch);
 
-		for (i = 0; i < 4; i++)
-			putch(input[i]);
-		printf("가 틀렸습니다.\n");
-	}
 }
