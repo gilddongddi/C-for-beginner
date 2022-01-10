5a8153e9c9dc00e528ff21aff3dfb8b5af655898
CM.Bae <gilddongii@gmail.com>
2022-01-10 23:50:37 +0900

    edit

--- Chapter14_Advanced/Section01_main()/14_1/14_1.c
+++ Chapter14_Advanced/Section01_main()/14_1/14_1.c
@@ -6,8 +6,8 @@ int main(int argc, char* argv[])							// 매개변수 지정
 {
 	int i;
 
-	printf(" argc 매개변수 개수 ==> %n \n", argc);				// 매개변수의 개수 출력
+	printf(" argc 매개변수 개수 ==> %d \n", argc-1);				// 매개변수의 개수 출력
 
-	for (i = 0; i < argc; i++)								// 매개변수 개수만큼 반복하여 매개변수의 내용을 출력
+	for (i = 1; i < argc; i++)								// 매개변수 개수만큼 반복하여 매개변수의 내용을 출력
 		printf(" argv[%d] 매개변수 ==> %s \n", i, argv[i]);
 }
