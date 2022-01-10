174f3093b697d01858ee6712f13fe5d895d35a08
CM.Bae <gilddongii@gmail.com>
2022-01-10 22:02:20 +0900

    struct pointer

--- Chapter13_struct_union_enum/Section01_struct/struct.c
+++ Chapter13_struct_union_enum/Section01_struct/struct.c
@@ -166,47 +166,99 @@
 
 
 // ===> 응용 13-5, 구조체 배열을 사용하여 변경
+//#include<stdio.h>
+//#include<string.h>
+//
+//int main()
+//{
+//	/*struct student {
+//		char name[10];
+//		int kor;
+//		int eng;
+//		float avg;
+//	};
+//
+//	struct student s[3];*/
+//
+//	/*struct student {
+//		char name[10];
+//		int kor;
+//		int eng;
+//		float avg;
+//	}s[3];*/
+//
+//	typedef struct _student {
+//		char name[10];
+//		int kor;
+//		int eng;
+//		float avg;
+//	}student;
+//
+//	student s[3];
+//
+//	int i;
+//
+//	strcpy(s[0].name, "Kim");
+//	s[0].kor = 90;
+//	s[0].eng = 80;
+//	s[0].avg = (s[0].kor + s[0].eng) / 2.0f;
+//
+//	strcpy(s[1].name, "Lee");
+//	s[1].kor = 70;
+//	s[1].eng = 80;
+//	s[1].avg = (s[1].kor + s[1].eng) / 2.0f;
+//
+//	strcpy(s[2].name, "Park");
+//	s[2].kor = 50;
+//	s[2].eng = 40;
+//	s[2].avg = (s[2].kor + s[2].eng) / 2.0f;
+//
+//
+//	printf("---구조체 배열 --\n");
+//	for (i = 0; i < 3; i++)
+//	{
+//		printf("학생 이름 ==> %s\n", s[i].name);
+//		printf("국어 점수 ==> %d\n", s[i].kor);
+//		printf("영어 점수 ==> %d\n", s[i].eng);
+//		printf("평균 점수 ==> %5.1f\n", s[i].avg);
+//		printf("\n");
+//	}
+//}
+
+
+
+// 기본13-6, 구조체 포인터 사용
 #include<stdio.h>
-#include<string.h>
 
 int main()
 {
-	struct student {
+	struct student
+	{
 		char name[10];
 		int kor;
 		int eng;
 		float avg;
 	};
 
-	struct student s[3];
+	struct student s;
+	struct student* p;		// 구조체 포인터 변수 p 선언
 
-	int i;
+	p = &s;
 
-	strcpy(s[0].name, "Kim");
-	s[0].kor = 90;
-	s[0].eng = 80;
-	s[0].avg = (s[0].kor + s[0].eng) / 2.0f;
+	printf("이름 입력 : ");
+	scanf("%s", p->name);			//name 자체가 주소이므로 &를 붙이지 않는다
 
-	strcpy(s[1].name, "Lee");
-	s[1].kor = 70;
-	s[1].eng = 80;
-	s[1].avg = (s[1].kor + s[1].eng) / 2.0f;
+	printf("국어 점수 : ");
+	scanf("%d", &p->kor);			//연산자 우선순위가 ->가 &보다 높기 때문에 괄호를 안 붙여도 됨
 
-	strcpy(s[2].name, "Park");
-	s[2].kor = 50;
-	s[2].eng = 40;
-	s[2].avg = (s[2].kor + s[2].eng) / 2.0f;
+	printf("엉어 점수 : ");
+	scanf("%d", &p->eng);
 
+	p->avg = (p->kor + p->eng) / 2.0f;
 
-	printf("---구조체 배열 --\n");
-	for (i = 0; i < 3; i++)
-	{
-		printf("학생 이름 ==> %s\n", s[i].name);
-		printf("국어 점수 ==> %d\n", s[i].kor);
-		printf("영어 점수 ==> %d\n", s[i].eng);
-		printf("평균 점수 ==> %5.1f\n", s[i].avg);
-		printf("\n");
-	}
+	printf("\n--구조체 포인터 활용 --\n");
+	printf("학생 이름 ==> %s\n", p->name);
+	printf("국어 점수 ==> %d\n", p->kor);
+	printf("영어 점수 ==> %d\n", p->eng);
+	printf("평균 점수 ==> %5.1f\n", p->avg);
 }
-
-
